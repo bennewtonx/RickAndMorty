7dd53db1929d093e1ed488ab56e3856f27df4606
Ben Newton <ben.newton112@gmail.com>
2023-07-28 09:40:36 +0100

    footer done

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css'
 import Header from './components/Header/Header';
 import Homepage from './pages/Homepage/Homepage';
-import Footer from './components/Footer/Footer';
 import About from './pages/About/About';
 import Episodes from './pages/Episodes/Episodes';
 import CharacterDetails from './pages/CharacterDetails/CharacterDetails';
+import Footer from './components/Footer/Footer';
 
 function App() {
 
